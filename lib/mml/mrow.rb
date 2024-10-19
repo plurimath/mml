7d9ad63fc9c99ce8226e888a77a6472036c8569c
suleman-uzair <suleman@workytical.com>
2024-10-19 21:28:21 +0500

    is_mrow for mrow specific check for Plurimath::Math::Formula class

--- lib/mml/mrow.rb
+++ lib/mml/mrow.rb
@@ -3,7 +3,6 @@
 module Mml
   class Munderover < Lutaml::Model::Serializable; end
   class Msubsup < Lutaml::Model::Serializable; end
-
   class Mfenced < Lutaml::Model::Serializable; end
   class Munder < Lutaml::Model::Serializable; end
   class Mstyle < Lutaml::Model::Serializable; end
@@ -22,6 +21,8 @@ module Mml
     attribute :mathcolor, :string
     attribute :mathbackground, :string
     attribute :dir, :string
+    attribute :is_mrow, :boolean, default: -> { true }
+
     Mml::Configuration::SUPPORTED_TAGS.each do |tag|
       attribute :"#{tag}_value", Mml.const_get(tag.capitalize), collection: true
     end
@@ -30,17 +31,11 @@ module Mml
       root "mrow"
       namespace "http://www.w3.org/1998/Math/MathML", nil
 
-      map_element "mi", to: :mi
-      map_element "mo", to: :mo
-      map_element "mn", to: :mn
-      map_element "ms", to: :ms
-      map_element "mrow", to: :mrow
-      map_element "mtext", to: :mtext
-      map_element "mstyle", to: :mstyle
-      map_element "munderover", to: :munderover
       map_attribute "dir", to: :dir
       map_attribute "mathcolor", to: :mathcolor
       map_attribute "mathbackground", to: :mathbackground
+      map_element "is_mrow", to: :is_mrow
+
       Mml::Configuration::SUPPORTED_TAGS.each do |tag|
         map_element tag, to: :"#{tag}_value"
       end
